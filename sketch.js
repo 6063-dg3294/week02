97ab507c3044138fe4865750b58b494d0b76aa0f
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-13 15:24:10 -0400

    added 2 ellipse and 1 rec

--- sketch.js
+++ sketch.js
@@ -1,5 +1,17 @@
 function setup() {
+   createCanvas(600, 600);
+   background(110,110,255);
+
 }
 
 function draw() {
+    stroke(0)
+    fill("orange")
+    ellipse(200, 200, 100, 100);
+    fill(150,20, 90)
+    ellipse(600, 200, 200, 200);
+    stroke("red")
+    strokeWeight(2)
+    rect(200, 200, 100, 10)
+
 }

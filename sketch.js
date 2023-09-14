096457eab1a01e30e713bfc956293d7b78f238f8
Difei (Amber) Gao <dg3294@nyu.edu>
2023-09-13 20:32:31 -0400

    changed to the lollipop image

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
+let eyewidth = 12;
+let facewidth = 300;
+let s = "LOLLIPOP"
+
 function setup() {
    createCanvas(600, 600);
-   background(110,110,255);
+   background(110,200,255);
 
 }
 
 function draw() {
-    stroke(0)
-    fill("orange")
-    ellipse(200, 200, 100, 100);
-    fill(150,20, 90)
-    ellipse(600, 200, 200, 200);
-    stroke("red")
-    strokeWeight(2)
-    rect(200, 200, 100, 10)
 
+    fill(36,74,207)
+    strokeWeight(0)
+    triangle(275, 50, 20, 550, 560, 550)
+    stroke("white")
+    strokeWeight(5)
+    fill(234,89,189)
+    ellipse(280,200,210,210);
+    fill("white")
+    rect(268, 305, 15, 220)
+    rect(280,200,80,10)
+    rect(250,220,80,10)
+    rect(200,210,80,10)
+    
+
+
+    
+    textSize(25)
+    textFont('Helvetica');
+    text(s, 25, 500,10,25)
+
+    fill(255);
+    ellipse(mouseX, mouseY, 100, 100)
 }
